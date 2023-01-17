2a34ba0de5eb0c6855c4903cde8460f99ecc65bf
tanvir07-cell <tanvir15-14402@diu.edu.bd>
2023-01-17 22:28:20 +0700

    for the env file

--- assignment-1-random-user-api/index.js
+++ assignment-1-random-user-api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-
+require("dotenv").config();
 const port = process.env.PORT || 8000;
 const usersRoutes = require("./routes/v1/users.routes");
 
